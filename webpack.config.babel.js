8dae1e0ac489ebf60fedd7ba7626bcccdacc11fa
fernfernfern <fernandomouco@gmail.com>
2017-12-12 20:12:38 +0000

    nice

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,18 +1,32 @@
 import path from 'path'
 
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+
 module.exports = {
-  entry: './src/index.js',
+  entry: ['babel-polyfill', './src/index.js'],
   output: {
-    filename: 'bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+
   module: {
     rules: [
       { test: /\.js?$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: ['babel-loader']
       },
-      { test: /\.css$/, use: 'css-loader'}
+			{ test: /\.json$/,
+				exclude: /node_modules/,
+				use: 'file-loader'
+			},
+      { test: /\.css$/, loader: 'style-loader!css-loader'}
     ]
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {from: './chrome/manifest.json', to: '.' },
+      {from: './public/inject.js', to: '.' },
+      {from: './public/icons', to: './icons' },
+    ], {copyUnmodified: true})
+  ]
 }
