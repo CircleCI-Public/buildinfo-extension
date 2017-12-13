d9cec12a5ebc86fbfc8eb8f8be31051b38ffac31
fernfernfern <fernandomouco@gmail.com>
2017-12-13 00:07:56 +0000

    on second thought - lets not build the zip every time during dev. new build command added

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,30 +3,33 @@ import path from 'path'
 import CopyWebpackPlugin from 'copy-webpack-plugin'
 import ZipPlugin from 'zip-webpack-plugin'
 
-module.exports = {
-  entry: ['babel-polyfill', './src/index.js'],
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
+export default (env={}) => {
 
-  module: {
-    rules: [
-      { test: /\.js?$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      { test: /\.css$/, loader: 'style-loader!css-loader'}
-    ]
-  },
-  plugins: [
-    new CopyWebpackPlugin([
-      {from: './chrome/manifest.json', to: '.' },
-      {from: './public/inject.js', to: '.' },
-      {from: './public/icons', to: './icons' },
-    ], {copyUnmodified: true}),
-    new ZipPlugin({
-      filename: 'extension.zip'
-    })
-  ]
+  return ({
+    entry: ['babel-polyfill', './src/index.js'],
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+      rules: [
+        { test: /\.js?$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+        { test: /\.css$/, loader: 'style-loader!css-loader'}
+      ]
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        {from: './chrome/manifest.json', to: '.' },
+        {from: './public/inject.js', to: '.' },
+        {from: './public/icons', to: './icons' },
+      ], {copyUnmodified: true}),
+      (env.zip && new ZipPlugin({
+        filename: 'extension.zip'
+      }))
+    ].filter(x => x)
+  })
 }
